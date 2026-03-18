ef675b85c7a29c88ffdf0c200d7f69fc0643d861
lipe-s-m <felipe.s.m1609@gmail.com>
2026-03-18 14:27:14 -0300

    cria commit

---
+++ src/main/java/com/felipe/CadastroDeClientes/Controller.java
@@ -0,0 +1,16 @@
+package com.felipe.CadastroDeClientes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hello World!";
+    }
+
+}
